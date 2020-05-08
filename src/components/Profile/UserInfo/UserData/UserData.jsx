3e6fc35ffb3a397b6712ce0186af77ca51607f37
Ddystopia <63151578+Ddystopia@users.noreply.github.com>
2020-05-08 23:24:02 +0300

    Refactor class comonent to hooc

--- src/components/Profile/UserInfo/UserData/UserData.jsx
+++ src/components/Profile/UserInfo/UserData/UserData.jsx
@@ -1,72 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classNames from "./UserData.module.css";
 
-class UserData extends React.Component {
-	state = { editMode: false, status: this.props.status };
-	statusOnChange = (e) => {
-		this.setState({ status: e.target.value });
+const UserData = (props) => {
+	const [editMode, setEditMode] = useState(false);
+	const [status, setStatus] = useState(props.status);
+	useEffect(() => {
+		setStatus(props.status)		
+	}, [props.status])
+	
+	const statusOnChange = (e) => {
+		setStatus(e.target.value);
 	};
 
-	anabelEditMode = () => {
-		if (this.props.authUserId !== this.props.profile.userId) return;
-		this.setState({ editMode: true });
+	const anabelEditMode = () => {
+		if (props.authUserId !== props.profile.userId) return;
+		setEditMode(true);
 	};
 
-	disableEditMode = () => {
-		this.setState({ editMode: false });
-		if (this.props.status === this.state.status) return;
-		if (this.state.status.length > 299) return;
-		this.props.updateUserStatus(this.state.status || "");
+	const disableEditMode = () => {
+		setEditMode(false);
+		if (props.status !== status) props.updateUserStatus(status || "");
 	};
-
-	componentDidUpdate(prevProps, prevState) {
-		if (prevProps.status !== this.props.status)
-			this.setState({ status: this.props.status });
-	}
-
-	render() {
-		const props = this.props;
-
-		return (
-			<article className={classNames.user_info_text}>
-				<h3>{props.profile.fullName || "-----"}</h3>
-				<div>About me: {props.profile.aboutMe || "-----"}</div>
-				<div className={classNames.statusContainer}>
-					Status:
-					{this.state.editMode ? (
-						<div className={classNames.textarea}>
-							<textarea
-								autoFocus={true}
-								onBlur={this.disableEditMode}
-								type="text"
-								value={this.state.status}
-								onChange={this.statusOnChange}
-							/>
-						</div>
-					) : (
-						<div
-							className={classNames.status}
-							onDoubleClick={this.anabelEditMode}
-						>
-							{props.status || "-----"}
-						</div>
-					)}
-				</div>
-				<div>
-					Looking for a job:
-					<div
-						className={classNames.circle}
-						style={{
-							backgroundColor: props.profile.lookingForAJob ? "green" : "red",
-						}}
-					></div>
-					{props.profile.lookingForAJobDescription ||
-					props.profile.lookingForAJob
-						? "-----"
-						: ""}
-				</div>
-			</article>
-		);
-	}
-}
+	
+	return (
+		<article className={classNames.user_info_text}>
+			<h3>{props.profile.fullName || "-----"}</h3>
+			<div>About me: {props.profile.aboutMe || "-----"}</div>
+			<div className={classNames.statusContainer}>
+				Status:
+				{editMode ? (
+					<div className={classNames.textarea}>
+						<textarea
+							autoFocus={true}
+							onBlur={disableEditMode}
+							type="text"
+							value={status}
+							onChange={statusOnChange}
+						/>
+					</div>
+				) : (
+					<div className={classNames.status} onDoubleClick={anabelEditMode}>
+						{props.status || "-----"}
+					</div>
+				)}
+			</div>
+			<div>
+				Looking for a job:
+				<div
+					className={classNames.circle}
+					style={{
+						backgroundColor: props.profile.lookingForAJob ? "green" : "red",
+					}}
+				></div>
+				{props.profile.lookingForAJobDescription || props.profile.lookingForAJob
+					? "-----"
+					: ""}
+			</div>
+		</article>
+	);
+};
 export default UserData;
